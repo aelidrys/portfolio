06b535015b8d6ab19f933e88251422194aa667c4
Ayoub Elidryssy <aelidrys@e1r12p5.1337.ma>
2025-01-01 13:41:02 +0100

    --02--

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,5 @@
+
+// ROLE Animation 
 const role = document.querySelector(".role")
 const loadtext = () => {
     role.textContent = "Full Stack Developer"
@@ -10,8 +12,8 @@ const loadtext = () => {
 loadtext()
 setInterval(loadtext,12000)
 
+// Active Page In Navbar
 const navbar_children =  document.querySelectorAll(".navbar div")
-
 navbar_children.forEach((el) => {
     el.addEventListener('click', (e) => {
         if (!el.classList.contains("active"))
@@ -27,8 +29,8 @@ navbar_children.forEach((el) => {
 });
 
 
+// Active Page In Small-Screen-Navbar
 const small_navbar_children =  document.querySelectorAll(".small-screen-navbar div")
-
 small_navbar_children.forEach((el) => {
     el.addEventListener('click', (e) => {
         if (!el.classList.contains("active"))
@@ -43,6 +45,8 @@ small_navbar_children.forEach((el) => {
     })
 });
 
+
+// Back To Top Botton
 const back_to_top = document.querySelector(".scroll_to_top")
 back_to_top.addEventListener("click", (e)  => {
     navbar_children.forEach((elem) => {
@@ -53,9 +57,10 @@ back_to_top.addEventListener("click", (e)  => {
     home.classList.add("active")
 })
 
+
+// Active and Deactive Small-Screen-Navbar
 const menu_icon = document.querySelector(".my_head .content .bars-active_")
 menu_icon.addEventListener("click", (e)=> {
-    console.log("**************")
     const navbar = document.querySelector(".my_head .content .small-screen-navbar")
     if (navbar.classList.contains("disp-block")){
         menu_icon.innerHTML = '<i class="fa-solid fa-bars"></i>'
@@ -67,5 +72,34 @@ menu_icon.addEventListener("click", (e)=> {
     }
 })
 
+// .Read More in About Me
+const readMoreBotton = document.querySelector(".about_me .read_more_button")
+const description = document.querySelector(".about_me .description")
+readMoreBotton.addEventListener("click", read_more.bind(null, readMoreBotton, description))
 
+
+
+// Read More in Services
+// First Service
+const fReadMoreBtn = document.querySelector(".services .card_container .card .read_more_button.first")
+const fDescription = document.querySelector(".services .card_container .card .description.first")
+fReadMoreBtn.addEventListener("click", read_more.bind(null, fReadMoreBtn, fDescription))
+// Second Service
+const sReadMoreBtn = document.querySelector(".services .card_container .card .read_more_button.second")
+const sDescription = document.querySelector(".services .card_container .card .description.second")
+sReadMoreBtn.addEventListener("click", read_more.bind(null, sReadMoreBtn, sDescription))
+// Third Service
+const tReadMoreBtn = document.querySelector(".services .card_container .card .read_more_button.third")
+const tDescription = document.querySelector(".services .card_container .card .description.third")
+tReadMoreBtn.addEventListener("click", read_more.bind(null, tReadMoreBtn, tDescription))
+
+function read_more(readMoreBotton, textDiv){
+    textDiv.classList.toggle("expanded")
+    if (textDiv.classList.contains("expanded")){
+        readMoreBotton.textContent = "Read Less"
+    }
+    else {
+        readMoreBotton.textContent = "Read More"
+    }
+}
 
